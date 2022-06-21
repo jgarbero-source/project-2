e7f02e5af7e3edc281ded11050586f4c0588d21d
John Garbero <john.garbero.itb@gmail.com>
2022-06-21 13:56:20 -0500

    favorite button!

--- src/components/CatContainer.js
+++ src/components/CatContainer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import CatItem from './CatItem';
 
 function CatContainer({ catData }) {
+
+  //create some if conditional that selects one div over the other? I don't think that's how this works. 
   return (
     <>
       <div className='container'>
@@ -9,7 +11,8 @@ function CatContainer({ catData }) {
       </div>
       {catData.map((cat) => (
         <CatItem key={cat.id} cat={cat} />
-      ))}
+      
+      ))} 
     </>
   );
 }
--- src/components/CatItem.js
+++ src/components/CatItem.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function CatItem({ cat }) {
+
+  const { id } = cat
+
+  function handleFavorite() {
+    cat.isFavorite=!cat.isFavorite
+    console.log(cat.isFavorite)
+    
+    fetch(`http://localhost:3000/cats/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({isFavorite: cat.isFavorite})
+    })
+  }
+
+  //we need to create a button that changes isFavorite in the db.json from false to true. If it is true, we put it in the favorites array.
+  //create a function that changes the data from false to true in app
+  //send this function down to the item and call it inside the handle favorite function. 
+  //have a patch request make it final. 
+
   return (
     <li>
       <h2>{cat.breed}</h2>
       <p>{cat.age}</p>
       <img src={cat.image} alt={cat.breed} />
+      <button onClick={handleFavorite} >Favorite</button>
     </li>
   );
 }
