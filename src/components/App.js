3b8855b94050bbbe0699e9da2aab9a82ec05d18f
Lyn <lynfinman@gmail.com>
2022-06-22 14:28:22 -0400

    Added OptionPicker for Form and Search

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Header from './Header';
-import NavBar from './NavBar';
-import Search from './Search';
-import Favorites from './Favorites';
-import Home from './Home';
-import Form from './Form';
-import '../assets/css/App.css';
+import React, { useEffect, useState } from "react";
+import { Route, Switch } from "react-router-dom";
+import Header from "./Header";
+import NavBar from "./NavBar";
+import Search from "./Search";
+import Favorites from "./Favorites";
+import Home from "./Home";
+import Form from "./Form";
+import "../assets/css/App.css";
 
 function App() {
   const [catData, setCatData] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/cats')
+    fetch("http://localhost:3000/cats")
       .then((res) => res.json())
       .then((data) => setCatData(data));
   }, []);
 
-  const favoriteCats = catData.filter(cats => cats.isFavorite === true )
-  console.log(favoriteCats)
+  const favoriteCats = catData.filter((cats) => cats.isFavorite === true);
+  console.log(favoriteCats);
 
   function handleCatUpdate(newFavCat) {
-    setCatData(catData.map(cat=> cat.id === newFavCat.id ? newFavCat : cat))
+    setCatData(
+      catData.map((cat) => (cat.id === newFavCat.id ? newFavCat : cat))
+    );
   }
 
   function addCat(newCat) {
-    setCatData([...catData, newCat])
+    setCatData([...catData, newCat]);
   }
 
   function deleteCat(catId) {
-    const updatedCats = catData.filter(cat => cat.id !== catId)
-    setCatData(updatedCats)
+    const updatedCats = catData.filter((cat) => cat.id !== catId);
+    setCatData(updatedCats);
   }
 
   return (
-    <div className='app'>
+    <div className="app">
       <Header />
       <NavBar />
       <Switch>
-        <Route exact path='/search'>
-          <Search catData={catData} handleCatUpdate={handleCatUpdate} deleteCat={deleteCat} />
+        <Route exact path="/search">
+          <Search
+            catData={catData}
+            handleCatUpdate={handleCatUpdate}
+            deleteCat={deleteCat}
+          />
         </Route>
-        <Route exact path='/favorites'>
-          <Favorites favoriteCats={favoriteCats} handleCatUpdate={handleCatUpdate} deleteCat={deleteCat} />
+        <Route exact path="/favorites">
+          <Favorites
+            favoriteCats={favoriteCats}
+            handleCatUpdate={handleCatUpdate}
+            deleteCat={deleteCat}
+          />
         </Route>
-        <Route exact path='/form'>
-          <Form addCat={addCat}/>
+        <Route exact path="/form">
+          <Form catData={catData} addCat={addCat} />
         </Route>
-        <Route exact path='/'>
+        <Route exact path="/">
           <Home />
         </Route>
       </Switch>
--- src/components/Search.js
+++ src/components/Search.js
@@ -9,50 +9,13 @@ function Search({ catData, handleCatUpdate, deleteCat }) {
     Gender: "All",
     Breed: "All",
   });
-  /*
-  const filterNames = Object.keys(filterChoice);
 
-  let filterOptions = {};
-  filterNames.forEach((option) => {
-    filterOptions = {
-      ...filterOptions,
-      [option]: catData
-        .map((cat) => cat[option.toLowerCase()])
-        .filter((val, ind, arr) => arr.indexOf(val) === ind),
-    };
-  });
-*/
   const handleSelection = (e) => {
     const { name, value } = e.target;
 
     setFilterChoice({ ...filterChoice, [name]: value });
   };
 
-  /*
-  const buildSelector = (choiceType) => {
-    return (
-      <div key={choiceType}>
-        <label key={choiceType + "label"}>{choiceType}</label>
-        <select
-          name={choiceType}
-          key={choiceType}
-          onChange={handleSelection}
-          value={filterChoice[choiceType]}
-        >
-          <option key={`all${choiceType}`}>All</option>
-          {filterOptions[choiceType].map((choice) => (
-            <option
-              key={`${choice}_${choiceType}`}
-              value={choice.toLowerCase()}
-            >
-              {choice}
-            </option>
-          ))}
-        </select>
-      </div>
-    );
-  };
-*/
   const filterCats = () => {
     // Filtering an object for just key:value pairs where value !== 'All'
     const asArray = Object.entries(filterChoice);
@@ -74,11 +37,6 @@ function Search({ catData, handleCatUpdate, deleteCat }) {
 
   return (
     <div className="filter" key="search">
-      {/*
-      <div className="filter" key="options">
-        {filterNames.map((option) => buildSelector(option))}
-      </div>
-       */}
       <OptionPicker
         catData={catData}
         filterChoice={filterChoice}
