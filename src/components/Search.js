ea607be3afb9aad9ccb88402669a2b302843b934
Lyn <lynfinman@gmail.com>
2022-06-21 23:22:45 -0400

    Fixed complaint about missing key by added div to buildSelector

--- src/components/Search.js
+++ src/components/Search.js
@@ -27,7 +27,7 @@ function Search({ catData, handleCatUpdate }) {
 
   const buildSelector = (choiceType) => {
     return (
-      <>
+      <div key={choiceType}>
         <label key={choiceType + "label"}>{choiceType}</label>
         <select
           name={choiceType}
@@ -37,12 +37,15 @@ function Search({ catData, handleCatUpdate }) {
         >
           <option key={`all${choiceType}`}>All</option>
           {filterOptions[choiceType].map((choice) => (
-            <option key={choice} value={choice.toLowerCase()}>
+            <option
+              key={`${choice}_${choiceType}`}
+              value={choice.toLowerCase()}
+            >
               {choice}
             </option>
           ))}
         </select>
-      </>
+      </div>
     );
   };
 
@@ -66,7 +69,7 @@ function Search({ catData, handleCatUpdate }) {
   };
 
   return (
-    <div className="filter">
+    <div className="filter" key="search">
       {filterNames.map((option) => buildSelector(option))}
       <CatContainer catData={filterCats()} handleCatUpdate={handleCatUpdate} />
     </div>
