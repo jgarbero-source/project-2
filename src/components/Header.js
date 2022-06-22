623390f6ec9cc0f6e0e739f4b750f3163afb1eb4
Devin Garcia <devingarcia5423@gmail.com>
2022-06-21 23:51:51 -0400

    made changes to catitem layout + added a headerpic

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,16 @@ import React from 'react';
 function Header() {
   return (
     <div className='nav-bar'>
-      <h2>Welcome to the Kitty Collective!</h2>
+      <h2>
+        <img
+          src='https://www.pngall.com/wp-content/uploads/2016/03/Cat-PNG-13.png'
+          alt='cat'
+          width='90'
+          height='64'
+          title='MEOW'
+        />
+        Welcome to the Kitty Collective!
+      </h2>
     </div>
   );
 }
