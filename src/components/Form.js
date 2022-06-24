9bfb876104bb780d2ab50617346dbf958596b2e4
Lyn <lynfinman@gmail.com>
2022-06-24 11:08:25 -0400

    cleaned out toLowerCase where no longer needed

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,30 +1,31 @@
-import React, { useState } from 'react';
-import Typography from '@mui/material/Typography';
-import { SelectChangeEvent } from '@mui/material/Select';
-import TextField from '@mui/material/TextField';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import OptionPicker from './OptionPicker';
-import Paper from '@mui/material/Paper';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
+import React, { useState } from "react";
+import Typography from "@mui/material/Typography";
+import { SelectChangeEvent } from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import OptionPicker from "./OptionPicker";
+import Paper from "@mui/material/Paper";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 
 function Form({ catData, addCat }) {
   const defaultFormData = {
-    age: '',
-    size: '',
-    gender: '',
-    breed: '',
+    age: "",
+    size: "",
+    gender: "",
+    breed: "",
   };
 
-  const [catImage, setCatImage] = useState('');
+  const [catImage, setCatImage] = useState("");
 
   const [filterChoice, setFilterChoice] = useState(defaultFormData);
 
   function handleImageChange(e) {
     setCatImage(e.target.value);
   }
+
   const handleSelection = (e: SelectChangeEvent) => {
     const { name, value } = e.target;
     setFilterChoice({ ...filterChoice, [name]: value });
@@ -32,25 +33,21 @@ function Form({ catData, addCat }) {
 
   function handleSubmit(e) {
     e.preventDefault();
+
     const newCat = {
       isFavorite: false,
-      age: filterChoice.age.charAt(0).toUpperCase() + filterChoice.age.slice(1),
-      gender:
-        filterChoice.gender.charAt(0).toUpperCase() +
-        filterChoice.gender.slice(1),
-      size:
-        filterChoice.size.charAt(0).toUpperCase() + filterChoice.size.slice(1),
+      age: filterChoice.age,
+      gender: filterChoice.gender,
+      size: filterChoice.size,
       image: catImage,
-      breed:
-        filterChoice.breed.charAt(0).toUpperCase() +
-        filterChoice.breed.slice(1),
+      breed: filterChoice.breed,
     };
     addCat(newCat);
 
     fetch(`${process.env.REACT_APP_API_URL}/cats`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(newCat),
     })
@@ -58,7 +55,7 @@ function Form({ catData, addCat }) {
       .then((oneCat) => addCat(oneCat));
 
     setFilterChoice(defaultFormData);
-    setCatImage('');
+    setCatImage("");
   }
   const allDataEntered =
     catImage &&
@@ -68,8 +65,8 @@ function Form({ catData, addCat }) {
     filterChoice.gender;
 
   return (
-    <Container maxWidth='lg' sx={{ paddingTop: '5px' }}>
-      <Typography variant='h4' gutterBottom component='div' paddi>
+    <Container maxWidth="lg" sx={{ paddingTop: "5px" }}>
+      <Typography variant="h4" gutterBottom component="div">
         Add a kitty!
       </Typography>
 
@@ -82,47 +79,47 @@ function Form({ catData, addCat }) {
           />
 
           <TextField
-            type='text'
-            name='image'
+            type="text"
+            name="image"
             value={catImage}
-            placeholder='Image URL'
-            label='Image URL'
-            variant='standard'
+            placeholder="Image URL"
+            label="Image URL"
+            variant="standard"
             onChange={handleImageChange}
             sx={{
-              paddingBottom: '5px',
-              marginLeft: '10px',
-              paddingRight: '10px',
-              width: '85%',
+              paddingBottom: "5px",
+              marginLeft: "10px",
+              paddingRight: "10px",
+              width: "85%",
             }}
           />
           <Card>
             {catImage ? (
               <CardMedia
-                component='img'
+                component="img"
                 src={catImage}
                 alt={filterChoice.breed}
-                height='500px'
+                height="500px"
               />
             ) : null}
             {filterChoice.age ? (
               <CardContent>
-                <Typography variant='subtitle1' gutterBottom component='div'>
+                <Typography variant="subtitle1" gutterBottom component="div">
                   {filterChoice.breed}
                 </Typography>
-                <Typography variant='subtitle2' gutterBottom component='div'>
+                <Typography variant="subtitle2" gutterBottom component="div">
                   {filterChoice.age}
                 </Typography>
-                <Typography variant='subtitle2' gutterBottom component='div'>
+                <Typography variant="subtitle2" gutterBottom component="div">
                   {filterChoice.size}
                 </Typography>
-                <Typography variant='subtitle2' gutterBottom component='div'>
+                <Typography variant="subtitle2" gutterBottom component="div">
                   {filterChoice.gender}
                 </Typography>
               </CardContent>
             ) : null}
           </Card>
-          <Button disabled={!allDataEntered} type='submit'>
+          <Button disabled={!allDataEntered} type="submit">
             Submit
           </Button>
         </form>
--- src/components/OptionPicker.js
+++ src/components/OptionPicker.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import React from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 function OptionPicker({ catData, filterChoice, onSelection }) {
   const filterNames = Object.keys(filterChoice);
@@ -19,20 +19,24 @@ function OptionPicker({ catData, filterChoice, onSelection }) {
 
   const buildSelector = (choiceType) => {
     return (
-      <FormControl variant='standard' sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel key={choiceType + 'label'} id={choiceType + 'label'}>
+      <FormControl
+        key={choiceType + "form"}
+        variant="standard"
+        sx={{ m: 1, minWidth: 120 }}
+      >
+        <InputLabel key={choiceType + "label"} id={choiceType + "label"}>
           {choiceType}
         </InputLabel>
         <Select
-          labelId={choiceType + 'label'}
-          id={choiceType + 'select'}
+          labelId={choiceType + "label"}
+          id={choiceType + "select"}
           label={choiceType}
           name={choiceType}
           key={choiceType}
           onChange={onSelection}
           value={filterChoice[choiceType]}
         >
-          <MenuItem key={`All_${choiceType}`} value=''>
+          <MenuItem key={`All_${choiceType}`} value="">
             <em>none</em>
           </MenuItem>
           {filterOptions[choiceType].map((choice) => (
